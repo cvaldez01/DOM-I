ddd08e9ac0c74fe0587e005cff259d94a81befa8
cvaldezo1 <cvaldez01@github.com>
2021-02-16 22:23:27 -0500

    strech goals almost done

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,16 +1,30 @@
 
 const button =document.createElement('button');
+const digits = document.getElementsByClassName('digits')
+const digit = document.getElementsByClassName('digit');
 const secondTens =document.getElementById('secondTens');
 const secondOnes =document.getElementById('secondOnes');
 const colon = document.getElementById('colon');
 const msHundreds = document.getElementById('msHundreds');
 const msTens =document.getElementById('msTens');
 
- let seconds = 0;
- function increase(){
-     for(let i=0 ; i < 60;i++){
-         if(i)
-     }
-     
- }
+ 
 
+const timer = setInterval(counter(),1000);
+ 
+function counter(){
+    let t = new Date();
+    let d = t.toLocaleTimeString();
+    secondTens.innerHTML = d;
+}
+
+ 
+
+
+
+    digits.appendChild(secondTens);
+    digits.appendChild(button);
+    digits.appendChild(secondOnes);
+    digits.appendChild(colon);
+    digits.appendChild(msHundreds);
+    digits.appendChild(msTens);
